15974a3cd4ecc8f0dca45778bdc5eb213261515a
Diego Leonardo Duran Castaños <a2019119811@estudiantes.upsa.edu.bo>
2021-03-20 11:31:13 -0400

    Se agrego el controlador de Add que suma dos numeros enteros

---
+++ Solucion Primer Parcial/Parcial1_DiegoDuran/Controllers/AddController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Parcial1_DiegoDuran.Controllers
+{
+    public class AddController : ApiController
+    {
+        [System.Web.Mvc.HttpPost]
+        [System.Web.Mvc.Route("Add")]
+        public int Add([FromHeader] int num_a, [FromHeader] int num_b) 
+        {
+            return num_a + num_b;
+        }
+    }
+}
